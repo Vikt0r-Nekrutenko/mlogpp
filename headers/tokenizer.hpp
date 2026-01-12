49eda67d1a724313956574c2da3135c0e35ab596
grhin0 <vikt0r.nekrut@outlook.com>
2026-01-12 22:22:08 +0200

    026

--- headers/tokenizer.hpp
+++ headers/tokenizer.hpp
@@ -19,6 +19,13 @@ namespace mlogpp
       Variable,
       Assigment,
       Operator,
+      KeywordMlog,
+      BlockStart,
+      BlockEnd, 
+      Endl, 
+      KeywordIf,
+      KeywordElse,
+      Cell
     };
     
     std::map<Type, std::string> TypeMap {
@@ -27,6 +34,13 @@ namespace mlogpp
       {Type::Variable, "Variable"},
       {Type::Assigment, "Assigment"},
       {Type::Operator, "Operator"},
+      {Type::KeywordMlog, "KeywordMlog"},
+      {Type::KeywordIf, "KeywordIf"},
+      {Type::KeywordElse, "KeywordElse"},
+      {Type::BlockStart, "BlockStart"},
+      {Type::BlockEnd, "BlockEnd"},
+      {Type::Endl, "Endl"},
+      {Type::Cell, "Cell"},
     };
     
     Token(size_t ln, const std::string &v, Type t)
@@ -77,7 +91,36 @@ namespace mlogpp
           tokens.push_back({lineNumber, string, Token::Type::String});
           string.clear();
         }
-      } 
+      } else if(match[2].matched) { // keywords
+          std::string keyword = match[2].str();
+          if(keyword == "and" || keyword == "or") {
+            tokens.push_back({lineNumber, keyword, Token::Type::Operator});
+          } else if(keyword == "if") {
+            tokens.push_back({lineNumber, keyword, Token::Type::KeywordIf});
+          } else if(keyword == "else") {
+            tokens.push_back({lineNumber, keyword, Token::Type::KeywordElse});
+          } else if(keyword == "mlog") {
+            tokens.push_back({lineNumber, keyword, Token::Type::KeywordMlog});
+          } else if(keyword.length() > 4 && std::string(keyword.begin(), keyword.begin()+4) == "cell") {
+            tokens.push_back({lineNumber, keyword, Token::Type::Cell});
+          }
+      } else if(match[3].matched) { // numbers
+          tokens.push_back({lineNumber, match[3].str(), Token::Type::Number});
+      } else if(match[4].matched) { // variables
+            tokens.push_back({lineNumber, match[4].str(), Token::Type::Variable});
+      } else if(match[5].matched) { // operators
+          std::string buffer = match[5].str();
+               if(buffer == "=")
+            tokens.push_back({lineNumber, buffer, Token::Type::Assigment});
+          else if(buffer == "{")
+            tokens.push_back({lineNumber, buffer, Token::Type::BlockStart});
+          else if(buffer == "}")
+            tokens.push_back({lineNumber, buffer, Token::Type::BlockEnd});
+          else if(buffer == ";")
+            tokens.push_back({lineNumber, buffer, Token::Type::Endl});
+          else
+            tokens.push_back({lineNumber, buffer, Token::Type::Operator});
+      }
     }
     return 0;
   }
